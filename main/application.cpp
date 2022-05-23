e1ce7ac1ce328715839178b9dd53234f3f8b0ec5
Szymon Zimnowoda <szymon.zimnowoda@gmail.com>
2022-05-23 22:58:12 +0200

    updated gpio wiring, wip moisture service

--- main/application.cpp
+++ main/application.cpp
@@ -18,10 +18,10 @@
 #include <esp_log.h>
 static const char* TAG = "Application";
 
-static const gpio_num_t SECTION_ONE = (gpio_num_t)12;
-static const gpio_num_t SECTION_TWO = (gpio_num_t)14;
-static const gpio_num_t SECTION_THREE = (gpio_num_t)27;
-static const gpio_num_t SECTION_FOUR = (gpio_num_t)26;
+static const gpio_num_t SECTION_ONE = (gpio_num_t)14;
+static const gpio_num_t SECTION_TWO = (gpio_num_t)27;
+static const gpio_num_t SECTION_THREE = (gpio_num_t)26;
+static const gpio_num_t SECTION_FOUR = (gpio_num_t)33;
 
 // TODO: add pulldown resistor?
 static const int TURN_ON = 0;
@@ -35,7 +35,7 @@ void setup_gpio() {
     // set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
     // bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_SEL_12;
+    io_conf.pin_bit_mask = GPIO_SEL_14;
     // disable pull-down mode
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     // disable pull-up mode
@@ -44,26 +44,26 @@ void setup_gpio() {
     gpio_config(&io_conf);
     gpio_set_level(SECTION_ONE, TURN_OFF);
 
-    io_conf.pin_bit_mask = GPIO_SEL_14;
+    io_conf.pin_bit_mask = GPIO_SEL_27;
     gpio_config(&io_conf);
     gpio_set_level(SECTION_TWO, TURN_OFF);
 
-    io_conf.pin_bit_mask = GPIO_SEL_27;
+    io_conf.pin_bit_mask = GPIO_SEL_26;
     gpio_config(&io_conf);
     gpio_set_level(SECTION_THREE, TURN_OFF);
 
-    io_conf.pin_bit_mask = GPIO_SEL_26;
+    io_conf.pin_bit_mask = GPIO_SEL_33;
     gpio_config(&io_conf);
     gpio_set_level(SECTION_FOUR, TURN_OFF);
 }
 
-static const adc2_channel_t MOISTURE_CHANNEL = ADC2_CHANNEL_3;
+static const adc1_channel_t MOISTURE_CHANNEL = ADC_CHANNEL_4; //GPIO32
 static const int AIR_DRY = 2590;
 static const int SOAKED_SOIL = 1200;
 
 void setup_adc() {
     // ADC2 config
-    ESP_ERROR_CHECK(adc2_config_channel_atten(MOISTURE_CHANNEL, ADC_ATTEN_DB_11));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(MOISTURE_CHANNEL, ADC_ATTEN_DB_11));
 }
 
 float calc_moisture(int adc_raw) {
@@ -80,16 +80,20 @@ float calc_moisture(int adc_raw) {
     return res;
 }
 
-void read_moisture() {
-    esp_err_t ret = ESP_OK;
-    static int adc_raw;
+void read_moisture(void* pvParameters) {
+    while (1) {
+        esp_err_t ret = ESP_OK;
+        static int adc_raw;
 
-    do {
-        ret = adc2_get_raw(MOISTURE_CHANNEL, (adc_bits_width_t)ADC_WIDTH_BIT_DEFAULT, &adc_raw);
-    } while (ret == ESP_ERR_INVALID_STATE);
-    ESP_ERROR_CHECK(ret);
+        do {
+            ret = adc1_get_raw(MOISTURE_CHANNEL, (adc_bits_width_t)ADC_WIDTH_BIT_DEFAULT, &adc_raw);
+        } while (ret == ESP_ERR_INVALID_STATE);
+        ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG, "raw  data: %d, moisture %f%%", adc_raw, calc_moisture(adc_raw));
+        ESP_LOGI(TAG, "raw  data: %d, moisture %f%%", adc_raw, calc_moisture(adc_raw));
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
 
 static const gpio_num_t RTC_SDA = (gpio_num_t)21;
@@ -148,6 +152,8 @@ void StartApplication() {
 
     xTaskCreate(get_clock, "get_clock", 1024 * 4, NULL, 2, NULL);
 
+    xTaskCreate(read_moisture, "read_moisture", 1024 * 4, NULL, 2, NULL);
+
     // read_moisture();
 
     gpio_num_t arr[4] = {SECTION_ONE, SECTION_TWO, SECTION_THREE, SECTION_FOUR};
@@ -155,7 +161,6 @@ void StartApplication() {
     for (int i = 0;; i++) {
         gpio_set_level(arr[i % 4], TURN_ON);
 
-        // read_moisture();
         vTaskDelay(pdMS_TO_TICKS(2000));
 
         gpio_set_level(arr[i % 4], TURN_OFF);
